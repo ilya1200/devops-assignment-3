86e2bfa8304e59af547903397274e66e818474f4
ilya <ilya1200@gmail.com>
2021-11-20 15:23:13 +0200

    refactor ex11 to print a chess_board img

--- ex11-Challenge.py
+++ ex11-Challenge.py
@@ -1,9 +1,19 @@
-from PIL import Image
+from PIL import Image, ImageColor
 
-img_name = "challenge.png"
-mode = "RGB"
-dim = (400, 500)
-color = (255, 0, 0)
+IMG_NAME = "chess_board.png"
+MODE = "RGB"
+SIZE = 8
+COLOR_1 = "red"
+COLOR_2 = "white"
 
-img = Image.new(mode, dim, color)
-img.save(img_name)
+DIM = (SIZE, SIZE)
+img = Image.new(MODE, DIM)
+pixels = img.load()
+
+for i in range(SIZE):
+    for j in range(SIZE):
+        CELL_COLOR = COLOR_1 if (i + j) % 2 else COLOR_2
+        pixels[i, j] = ImageColor.getcolor(CELL_COLOR, MODE)
+
+img = img.resize((16 * SIZE, 16 * SIZE), Image.NEAREST)
+img.save(IMG_NAME)
